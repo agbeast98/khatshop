4fbd5dfccdb6318e074297dec3b4d9cd03fe3250
agbeast98 <mohammadabdollahi80808@gmail.com>
2024-11-21 02:51:09 +0330

    Initial commit

---
+++ store/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+
+Route::get('/', function () {
+    return view('welcome');
+});
